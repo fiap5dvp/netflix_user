e987f3ba476690473a50b76e2ed341c8a56c5b51
Lucas <lucas.lima@spsconsultoria.com.br>
2021-04-16 20:28:08 -0300

    Remoção eureka

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 require("dotenv").config();
 
 const server = require("./server");
-const eureka = require("./Eureka");
 
 const port = process.env.PORT;
 
 server.listen(port, () => {
-  eureka.register({
-    app: process.env.SERVICE_NAME,
-    hostName: process.env.HOST_NAME,
-    port: process.env.PORT,
-    healthCheckUrl: process.env.HELTH_CHECK_URL,
-    eurekaHost: process.env.EUREKA_HOST,
-    eurekaPort: process.env.EUREKA_PORT,
-  });
-
   console.log("Service is running. Port " + port);
 });
